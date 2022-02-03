c19de222147ac5b989fde122918101e7576713e0
Carlos Henrique <40575281+chss734@users.noreply.github.com>
2022-02-03 01:29:47 -0300

    Printoz
    monitoramento de impressoras

---
+++ printoz/index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PRINTS RICOH OSASCO</title>
+    <link rel="stylesheet" href="css/default.css" type="text/css">
+    <script src="js/script.js"></script>
+    <link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@300&display=swap" rel="stylesheet">
+</head>
+<body>
+<section class="main_container">
+<!-- lado esq -->
+<navbar>
+    <article class="logo">
+        <img src="img/logo.svg"/>
+    </article>
+    <article class="menusItens">
+    <a href="#"><div class="item ativo"><img src="img/home.svg"> Dashboard</div></a>
+    <a href="#"><div class="item offl"><img src="img/Frame.svg"> Impressoras</div></a>
+
+    
+    </article>
+
+
+
+
+
+</navbar>
+<!-- lado dir -->
+<header>
+<article class="topo">
+    <article class="form">
+            <article class="input">
+                <form action="" method="post">
+                    <input type="text" class="search" placeholder="Pesquisar impressora">
+                    <input type="image" src="img/search.svg" class="icon_search">
+                </form>
+            </article>
+            <article class="user" >
+            <img src="img/user.svg">
+            <article class="nameuser" onmouseover="abrir()">Carlos Henrique<img src="img/arrowdown.svg" style="width:8px; height:4px;" ></article>
+                <article class="expandir_user" id="expandir_user">
+                <a href="#"><div class="item offl"><img src="img/Frame.svg"> Impressoras</div></a>
+                </article>
+            </article><!--  user -->
+    </article>
+
+
+
+
+
+
+</article><!-- topo -->
+
+</header>
+</section><!-- main_container -->
+
+
+
+
+</body>
+</html>
---
+++ printoz/js/script.js
@@ -0,0 +1,18 @@
+function abrir(){
+    document.getElementById("expandir_user").style.display = "flex";
+    document.getElementById("expandir_user").style.transition = "all 2s";
+}
+function fechar(){
+    document.getElementById("expandir_user").style.display = "none";
+}
+
+
+
+
+
+
+
+
+
+
+
